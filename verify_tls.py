4493a310fbd86801732e1a58f12bcccb1bfcf99b
Gokul <166112136+Gokul-Rajaraman@users.noreply.github.com>
2024-08-13 10:07:26 +0530

    Update verify_tls.py

--- verify_tls.py
+++ verify_tls.py
@@ -4,15 +4,19 @@ import requests
 
 
 def main():
-    version = requests.get('https://www.howsmyssl.com/a/check').json()['tls_version']
+    version = requests.get("https://www.howsmyssl.com/a/check").json()["tls_version"]
     if version < "TLS 1.2":
         url = "https://pyfound.blogspot.com/2017/01/time-to-upgrade-your-python-tls-v12.html"
-        print("Error: The Stripe API requires TLS version 1.2, you are running {version}"
-              "\n\nPlease see {url}\nfor instructions to update your environment.".format(
-                  version=version, url=url))
+        print(
+            "Error: The Stripe API requires TLS version 1.2, you are running {version}"
+            "\n\nPlease see {url}\nfor instructions to update your environment.".format(
+                version=version, url=url
+            )
+        )
         exit(1)
     print("TLS 1.2 supported; no action required.")
 
 
 if __name__ == "__main__":
     main()
+print("hello")
